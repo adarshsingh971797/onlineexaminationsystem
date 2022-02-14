2675d9d9f463360e5fac6e61a33d724d364207b7
adarshsingh971797 <99664067+adarshsingh971797@users.noreply.github.com>
2022-02-14 21:52:28 +0530

    Update dbConnection.php

--- dbConnection.php
+++ dbConnection.php
@@ -1,5 +1,10 @@
 <?php
 //all the variables defined here are accessible in all the files that include this one
-$con= new mysqli('localhost','root','','project')or die("Could not connect to mysql".mysqli_error($con));
+$server = "sql671.main-hosting.eu";
+$user = "u429173902_exam";
+$password = "Digitron@123";
+$db = "u429173902_exam";
+
+$con= new mysqli($server, $user, $password, $db)or die("Could not connect to mysql".mysqli_error($con));
 
 ?>
